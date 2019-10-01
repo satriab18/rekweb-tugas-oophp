7e256d90eea390f2c5f986d4700f188e7a2bb95b
Satria Bagus <satria.bagus18@gmail.com>
2019-10-02 00:52:14 +0700

    Menambah produk

--- OOPHP/Property-Method/produk.php
+++ OOPHP/Property-Method/produk.php
@@ -7,13 +7,20 @@ class Produk {
             $harga = 0;
 }
 
-$produk1 = new Produk();
-$produk1->judul = "Naruto";
-var_dump($produk1);
+// $produk1 = new Produk();
+// $produk1->judul = "Naruto";
+// var_dump($produk1);
 
-$produk2 = new Produk();
-$produk2->judul = "Uncharted";
-$produk2->tambahProperty = "hahaha";
-var_dump($produk2);
+// $produk2 = new Produk();
+// $produk2->judul = "Uncharted";
+// $produk2->tambahProperty = "hahaha";
+// var_dump($produk2);
 
+$produk3 = new Produk();
+$produk3->judul = "Naruto";
+$produk3->penulis = "Masashi Kishimoto";
+$produk3->penerbit = "Shonen Jump";
+$produk3->harga = 30000;
+
+echo "Komik : $produk3->penulis, $produk3->penerbit";
 ?>
