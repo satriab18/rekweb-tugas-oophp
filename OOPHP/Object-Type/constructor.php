91eb01b001691abc5afd74e1c6d7e2d9cc580efa
Satria Bagus <satria.bagus18@gmail.com>
2019-10-02 03:19:09 +0700

    Mengisi parameter harus produk pada fungsi cetak

--- OOPHP/Object-Type/constructor.php
+++ OOPHP/Object-Type/constructor.php
@@ -32,7 +32,7 @@ $produk2 = new Produk("Uncharted", "Neil Druckmann", "Sony Computer", 250000);
 echo "Komik : " . $produk1->getLabel();
 echo "<br>";
 echo "Game : " . $produk2->getLabel();
-
+echo "<br>";
 $infoProduk1 = new CetakInfoProduk();
 echo $infoProduk1->cetak($produk1);
 ?>
