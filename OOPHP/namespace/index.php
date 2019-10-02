f1ad249c805dd3b837b7af15654252077fd54343
Satria Bagus <satria.bagus18@gmail.com>
2019-10-02 19:51:49 +0700

    Membuat folder namespace

---
+++ OOPHP/namespace/index.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once 'App/init.php';
+
+$produk1 = new Komik("Naruto", "Masashi Kishimotor", "Shonen Jump", 30000, 100, "Komik");
+$produk2 = new Game("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 50, "Game");
+
+$cetakProduk = new CetakInfoProduk();
+$cetakProduk->tambahProduk($produk1);
+$cetakProduk->tambahProduk($produk2);
+echo $cetakProduk->cetak();
+
+echo "<hr>";
+?>
