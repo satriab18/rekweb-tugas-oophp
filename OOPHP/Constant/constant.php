ad80167fc1a21046e49e97bc0ec105816780551b
Satria Bagus <satria.bagus18@gmail.com>
2019-10-02 16:50:37 +0700

    Mencoba constant

---
+++ OOPHP/Constant/constant.php
@@ -0,0 +1,19 @@
+<?php
+
+// define('NAMA', 'Satria Bagus');
+// echo NAMA;
+
+// echo "<br>";
+
+// const UMUR = 20;
+// echo UMUR;
+
+class Coba {
+    const NAMA = 'Satria Bagus';
+}
+
+echo Coba::NAMA;
+
+
+
+?>
