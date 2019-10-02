94dd4a2441765e64d0c0262c13e268900ab699b1
Satria Bagus <satria.bagus18@gmail.com>
2019-10-02 20:08:23 +0700

    Menggunakan namespace pada Produk dan Services

--- OOPHP/namespace/index.php
+++ OOPHP/namespace/index.php
@@ -2,13 +2,17 @@
 
 require_once 'App/init.php';
 
-$produk1 = new Komik("Naruto", "Masashi Kishimotor", "Shonen Jump", 30000, 100, "Komik");
-$produk2 = new Game("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 50, "Game");
+// $produk1 = new Komik("Naruto", "Masashi Kishimotor", "Shonen Jump", 30000, 100, "Komik");
+// $produk2 = new Game("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 50, "Game");
 
-$cetakProduk = new CetakInfoProduk();
-$cetakProduk->tambahProduk($produk1);
-$cetakProduk->tambahProduk($produk2);
-echo $cetakProduk->cetak();
+// $cetakProduk = new CetakInfoProduk();
+// $cetakProduk->tambahProduk($produk1);
+// $cetakProduk->tambahProduk($produk2);
+// echo $cetakProduk->cetak();
 
-echo "<hr>";
+// echo "<hr>";
+
+new App\Produk\User(); 
+echo "<br>";
+new App\Services\User(); 
 ?>
