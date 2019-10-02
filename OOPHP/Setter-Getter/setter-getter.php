622eb57ecb444264668801e7e2fb54ad0de43ad8
Satria Bagus <satria.bagus18@gmail.com>
2019-10-02 14:34:29 +0700

    memperbaiki seluruh fungi setter dan getter

--- OOPHP/Setter-Getter/setter-getter.php
+++ OOPHP/Setter-Getter/setter-getter.php
@@ -3,12 +3,10 @@
 class Produk {
     private $judul,
             $penulis,
-            $penerbit;
+            $penerbit,
+            $harga;
 
     protected $diskon = 0;
-
-    private $harga;
-
 
     public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0){
         $this->judul = $judul;
@@ -28,6 +26,30 @@ class Produk {
             return $this->judul;
     }
 
+    public function setPenulis($penulis){
+            $this->penulis = $penulis;
+    }
+
+    public function getPenulis(){
+            return $this->penulis;
+    }
+
+    public function setPenerbit($penerbit){
+            $this->penerbit=$penerbit;
+    }
+
+    public function getPenerbit(){
+            return $this->penerbit;
+    }
+
+    public function setHarga($harga){
+            $this->harga = $harga;
+    }
+
+    public function getHarga(){
+        return $this->harga - ($this->harga * $this->diskon / 100);   
+   }
+
     public function getLabel(){
                 return "$this->penulis, $this->penerbit";
         }
@@ -36,10 +58,15 @@ class Produk {
                 $str = "{$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
                 return $str;
         }
+        
+        public function setDiskon($diskon){
+                $this->diskon = $diskon;
+        }
 
-        public function getHarga(){
-                return $this->harga - ($this->harga * $this->diskon / 100);   
-           }
+        public function getDiskon(){
+               return $this->diskon;
+        }
+
 }
 
 class Komik extends Produk {
@@ -71,10 +98,7 @@ class Game extends Produk {
                 $str = "Game : " . parent::getInfoProduk() . " - {$this->waktuMain} Jam";
                 return $str;
         }
-        
-        public function setDiskon($diskon){
-                $this->diskon = $diskon;
-        }
+
 }
 
 class CetakInfoProduk {
